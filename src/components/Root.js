9d66a40f80e6ba5f8c71e5c237586db049e47528
Dmitry Umbras <freeudv@mail.ru>
2018-03-13 16:11:32 +0300

    lesson 6 - add text inputs adn basic styles

---
+++ src/components/Root.js
@@ -0,0 +1,13 @@
+import React from "react"
+import { View } from "react-native"
+import SignIn from "./auth/SignIn"
+
+function Root() {
+  return (
+    <View>
+      <SignIn />
+    </View>
+  )
+}
+
+export default Root
---
+++ src/components/auth/SignIn.js
@@ -0,0 +1,60 @@
+import React, { Component } from "react"
+import { View, Text, TextInput, Platform } from "react-native"
+
+export default class SignIn extends Component {
+  state = {
+    email: "",
+    password: ""
+  }
+
+  setEmail = email => {
+    this.setState({
+      email
+    })
+  }
+
+  setPassword = password => {
+    this.setState({
+      password
+    })
+  }
+
+  render() {
+    const { email, password } = this.state
+    return (
+      <View>
+        <Text style={styles.header}>Please Sign In</Text>
+        <Text>Email:</Text>
+        <TextInput
+          value={email}
+          onChangeText={this.setEmail}
+          style={styles.input}
+          keyboardType="email-address"
+        />
+        <Text>Password:</Text>
+        <TextInput
+          value={password}
+          onChangeText={this.setPassword}
+          style={styles.input}
+          secureTextEntry
+        />
+      </View>
+    )
+  }
+}
+
+const styles = {
+  header: {
+    fontSize: 20,
+    fontWeight: "bold"
+  },
+  input: {
+    ...Platform.select({
+      ios: {
+        borderBottomColor: "#000",
+        borderBottomWidth: 1
+      },
+      android: {}
+    })
+  }
+}
